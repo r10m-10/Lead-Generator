3b61906f2bffde27f04e6357227bf79d2bd1605c
R10M <reyaanshsharma11@gmail.com>
2026-09-13 16:20:44 +0530

    batabase boilerplate

--- backend/database.py
+++ backend/database.py
@@ -2,3 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models.base import Base
 
+db_url = "sqlite:///../data/platform.db"
+
+engine = create_engine(db_url)
+
+SessionLocal = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
